066fc53da4f311f7c20da9ec602290db7e97fbeb
abirtasrif <abirtasrif@gmail.com>
2023-10-05 13:36:37 +0600

    updated specialist controller

--- src/controllers/specialist.controller.ts
+++ src/controllers/specialist.controller.ts
@@ -2,6 +2,7 @@ import { Request, Response } from 'express';
 import { handleError } from '../errors/handle.error';
 import SpecialistModel from '../models/specialist.model';
 import mongoose from 'mongoose';
+import BeautyPackageModel from '../models/beautyPackage.model';
 
 export default class SpecialistController {
   constructor() {}
@@ -40,6 +41,12 @@ export default class SpecialistController {
     try {
       const { name, designation, bio, photoUrl, dateOfBirth } = req.body;
 
+      const { bid } = req.params;
+
+      if (!mongoose.Types.ObjectId.isValid(bid)) {
+        res.status(404).json({ message: 'Beauty package not found' });
+      }
+
       await Promise.resolve().then(async () => {
         const specialist = await SpecialistModel.create({
           name,
@@ -47,6 +54,12 @@ export default class SpecialistController {
           bio,
           photoUrl,
           dateOfBirth,
+        });
+
+        await BeautyPackageModel.findByIdAndUpdate(bid, {
+          $addToSet: {
+            specialists: specialist._id,
+          },
         });
 
         res.status(200).json(specialist);
